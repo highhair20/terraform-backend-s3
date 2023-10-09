e3494094683849bf2e038d60cedccba2d46ea957
jasoninmarket <jason@inmarket.com>
2023-10-08 19:39:38 -0700

    Initial commit for terraform backend

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # terraform-backend-s3
+
+Instead, use the variable “shared_credentials_file” and if you have several profiles like in my case the variable “profile”.
---
+++ dynamodb.tf
@@ -0,0 +1,16 @@
+/*
+  The primary key to be used to lock the state in dynamoDB must be called LockID and must be a “string” type (S).
+/*
+resource "aws_dynamodb_table" "terraform-lock" {
+    name           = "terraform_state"
+    read_capacity  = 5
+    write_capacity = 5
+    hash_key       = "LockID"
+    attribute {
+        name = "LockID"
+        type = "S"
+    }
+    tags = {
+        "Name" = "DynamoDB Terraform State Lock Table"
+    }
+}
